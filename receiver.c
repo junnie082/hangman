21ca109ff4cb304688b07763604e7afe8a365511
Hyojeong Jun <junnie082@gmail.com>
2023-12-04 23:11:52 +0900

    fix: fixed the error of receiver/sender not working as intended

--- receiver.c
+++ receiver.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define DEFINED_KEY 0x10101010
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     int msg_qid;
     struct {
@@ -24,7 +24,7 @@ main(int argc, char **argv)
     } 
 
     int chance = 7; 
-    char guess, word[100]; 
+    char guess[100], word[100]; 
     
     strcpy(word, msg.content);
 
@@ -45,19 +45,23 @@ main(int argc, char **argv)
     while (chance != 0) {
         number++;
         printf("\n영어 알파벳(소문자만 입력하세요)을 고르세요. 기회는 %d 번 주어집니다.\n", chance);
-        scanf(" %c", &guess); 
-        
+        scanf(" %s", guess); 
+        while (strlen(guess) != 1) {
+            printf("오직 하나의 영어 소문자만 입력하세요.\n"); 
+            scanf(" %s", guess); 
+        }
+
         int correct = 0; 
         for (int i = 0; i < size; i++) {
-            if (word[i] == guess) {
-                answerSheet[i] = guess;
+            if (word[i] == guess[0]) {
+                answerSheet[i] = guess[0];
                 correct = 1;
             }
         }
 
         if (correct == 0) {
             chance--;
-            printf("틀렸습니다! 기회가 %d 로 줄어듭니다. \n", chance);
+            printf("***** 틀렸습니다! *****기회가 %d 로 줄어듭니다. \n", chance);
         }
 
         // 출력하기. 
@@ -70,7 +74,7 @@ main(int argc, char **argv)
 
         // 글자를 단 하나도 맞추지 못했을 때. 
         if (correct == 1) {
-            printf("%d번 만에 정답을 맞추었습니다!\n", number); 
+            printf("------> ***** %d번 만에 정답을 맞추었습니다! ***** <------\n", number); 
 
             exit(0); 
         }
